3ae9686e70ea1f10c2a1dbb9bd5a5a50fa6f8e87
winschen98 <winston.chen98@gmail.com>
2021-04-23 12:57:56 -0700

    Created product model

--- fan_apparel/models.py
+++ fan_apparel/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Product(models.Model):
+    # image =
+    _id = models.AutoField(primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=200, null=True, blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=True, blank=True)
+    category = models.CharField(max_length=200, null=True, blank=True)
+    details = models.TextField(null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
