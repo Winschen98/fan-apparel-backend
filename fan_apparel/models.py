acd59cda3cdae85bf32c2a693716452a55c202ea
winschen98 <winston.chen98@gmail.com>
2021-04-23 13:07:38 -0700

    Created Order model

--- fan_apparel/models.py
+++ fan_apparel/models.py
@@ -17,3 +17,21 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
+
+class Order(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    paymentMethod = 
+    taxCost = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    shippingCost = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    totalCost = models.DecimalField(
+        max_digits=7, decimal_places=2, null=True, blank=True)
+    Paid = models.BooleanField(default=False)
+    paidAt = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+    createdAt = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self): 
+        return str(self.createdAt)
+
