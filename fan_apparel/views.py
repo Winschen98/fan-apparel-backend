2bafeddd6fe29bb65e13a7d617f8a99b4c1683ca
winschen98 <winston.chen98@gmail.com>
2021-05-30 20:19:21 -0400

    Added endpoint for user info updates

--- fan_apparel/views.py
+++ fan_apparel/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 
 from .models import Product, User
@@ -56,6 +57,7 @@ def registerUser(request):
 
 # users views
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def getUserProfile(request):
     user = request.user
     serializer = UserSerializer(user, many=False)
